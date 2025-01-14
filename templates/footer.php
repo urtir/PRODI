1d46028ad37e9301eed210a19546637beb51c40f
johantgg <johantgg113@gmail.com>
2025-01-15 01:09:36 +0700

    Update footer.php
    link sosmed pada footer done

--- templates/footer.php
+++ templates/footer.php
@@ -9,22 +9,21 @@
                 <h5 class="mb-3">Program Studi Informatika</h5>
                 <p class="mb-1">Email: info@informatika.edu</p>
                 <p class="mb-3">Phone: +62 123 4567 890</p>
-                <div class="social-links">
-                    <a href="#" class="text-white me-2"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="text-white me-2"><i class="fab fa-twitter"></i></a>
-                    <a href="#" class="text-white me-2"><i class="fab fa-instagram"></i></a>
-
-                </div>
+            <div class="social-links">
+                <a href="https://web.facebook.com/unhanhumas.idu.ac.id" target="_blank" class="text-white me-2"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://x.com/Unhan_RI" target="_blank" class="text-white me-2"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/unhan_informatics?igsh=MXAzcjFzeTlqdTcyZA==" target="_blank" class="text-white me-2"><i class="fab fa-instagram"></i></a>
+            </div>
             </div>
             
             <div class="col-md-4">
                 <h5 class="mb-3">Quick Links</h5>
                 <ul class="list-unstyled">
-                    <li class="mb-2"><a href="/courses" class="text-white text-decoration-none">Mata Kuliah</a></li>
-                    <li class="mb-2"><a href="/events" class="text-white text-decoration-none">Acara</a></li>
-                    <li class="mb-2"><a href="/admission" class="text-white text-decoration-none">Buletin</a></li>
-                    <li class="mb-2"><a href="/contact" class="text-white text-decoration-none">Kontak</a></li>
-                </ul>
+                    <li class="mb-2"><a href="courses.php" class="text-white text-decoration-none">Mata Kuliah</a></li>
+                    <li class="mb-2"><a href="events.php" class="text-white text-decoration-none">Acara</a></li>
+                    <li class="mb-2"><a href="buletin.php" class="text-white text-decoration-none">FAQ</a></li>
+                    <li class="mb-2"><a href="contact.php" class="text-white text-decoration-none">Hubungi</a></li>
+                 </ul>
             </div>
 
             
